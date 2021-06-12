c09bbc02be81a6e4eaeca68fba2ad00a792956c5
afanner <fanner@wisc.edu>
2021-06-11 20:05:30 -0500

    adding a few more tests

--- AppBL/GACDTests/GACDUnitTests.cs
+++ AppBL/GACDTests/GACDUnitTests.cs
@@ -79,7 +79,76 @@ namespace GACDTests
             }
         }
         [Fact]
-        public async Task OverallLeaderBoardShouldReturnUsers()
+        public async Task OverallLeaderBoardShouldReturnAnyNumberofUsers()
+        {
+            using (var context = new GACDDBContext(options))
+            {
+                IUserBL userBL = new UserBL(context);
+                ICategoryBL categoryBL = new CategoryBL(context);
+                IUserStatBL userStatBL = new UserStatBL(context);
+                Category category = new Category();
+                category.Name = 1;
+                await categoryBL.AddCategory(category);
+                Category category1 = new Category();
+                category1.Name = 2;
+                await categoryBL.AddCategory(category1);
+                Category category2 = new Category();
+                category2.Name = 3;
+                await categoryBL.AddCategory(category2);
+                User user = new User();
+                user.Auth0Id = "test";
+                await userBL.AddUser(user);
+                User user1 = new User();
+                user1.Auth0Id = "test1";
+                await userBL.AddUser(user1);
+                User user2 = new User();
+                user2.Auth0Id = "test2";
+                await userBL.AddUser(user2);
+                TypeTest testToBeInserted = await userStatBL.SaveTypeTest(1, 50, 100, DateTime.Now);
+                await userStatBL.AddTestUpdateStat(1, 1, testToBeInserted);
+                testToBeInserted = await userStatBL.SaveTypeTest(2, 50, 100, DateTime.Now);
+                await userStatBL.AddTestUpdateStat(2, 2, testToBeInserted);
+                testToBeInserted = await userStatBL.SaveTypeTest(3, 50, 100, DateTime.Now);
+                await userStatBL.AddTestUpdateStat(3, 3, testToBeInserted);
+                bool expected = true;
+                bool actual = (await userStatBL.GetOverallBestUsers()).Count > 0;
+                Assert.Equal(expected, actual);
+            }
+        }
+        [Fact]
+        public async Task CategoryLeaderBoardShouldReturnAnyNumberofUsers()
+        {
+            using (var context = new GACDDBContext(options))
+            {
+                IUserBL userBL = new UserBL(context);
+                ICategoryBL categoryBL = new CategoryBL(context);
+                IUserStatBL userStatBL = new UserStatBL(context);
+                Category category = new Category();
+                category.Name = 1;
+                await categoryBL.AddCategory(category);
+
+                User user = new User();
+                user.Auth0Id = "test";
+                await userBL.AddUser(user);
+                User user1 = new User();
+                user1.Auth0Id = "test1";
+                await userBL.AddUser(user1);
+                User user2 = new User();
+                user2.Auth0Id = "test2";
+                await userBL.AddUser(user2);
+                TypeTest testToBeInserted = await userStatBL.SaveTypeTest(1, 50, 100, DateTime.Now);
+                await userStatBL.AddTestUpdateStat(1, 1, testToBeInserted);
+                TypeTest testToBeInserted1 = await userStatBL.SaveTypeTest(2, 50, 100, DateTime.Now);
+                await userStatBL.AddTestUpdateStat(2, 1, testToBeInserted1);
+                TypeTest testToBeInserted2 = await userStatBL.SaveTypeTest(3, 50, 100, DateTime.Now);
+                await userStatBL.AddTestUpdateStat(3, 1, testToBeInserted2);
+                bool expected = true;
+                bool actual = (await userStatBL.GetBestUsersForCategory(1)).Count > 0;
+                Assert.Equal(expected, actual);
+            }
+        }
+        [Fact]
+        public async Task OverallLeaderBoardShouldReturnCorrectNumberofUsers()
         {
             using(var context = new GACDDBContext(options))
             {
@@ -116,7 +185,7 @@ namespace GACDTests
             }
         }
         [Fact]
-        public async Task CategoryLeaderBoardShouldReturnUsers()
+        public async Task CategoryLeaderBoardShouldReturnCorrectNumberofUsers()
         {
             using (var context = new GACDDBContext(options))
             {
@@ -145,6 +214,30 @@ namespace GACDTests
                 int expected = 3;
                 int actual = (await userStatBL.GetBestUsersForCategory(1)).Count;
                 Assert.Equal(expected, actual);
+            }
+        }
+        [Fact]
+        public async Task AverageWPMShouldBeAverage()
+        {
+            using (var context = new GACDDBContext(options))
+            {
+                User user = new User();
+                user.Auth0Id = "test";
+                IUserBL userBL = new UserBL(context);
+                ICategoryBL categoryBL = new CategoryBL(context);
+                IUserStatBL userStatBL = new UserStatBL(context);
+                Category category = new Category();
+                category.Name = 1;
+                await categoryBL.AddCategory(category);
+                await userBL.AddUser(user);
+                Double avgExpected;
+                TypeTest testToBeInserted = await userStatBL.SaveTypeTest(1, 50, 100, DateTime.Now);
+                avgExpected = testToBeInserted.WPM/2;
+                await userStatBL.AddTestUpdateStat(1, 1, testToBeInserted);
+                TypeTest testToBeInserted1 = await userStatBL.SaveTypeTest(1, 50, 100, DateTime.Now);
+                avgExpected += testToBeInserted1.WPM/2;
+                Double actual = (await userStatBL.AddTestUpdateStat(1, 1, testToBeInserted)).AverageWPM;
+                Assert.Equal(avgExpected, actual);
             }
         }
         private void Seed()
