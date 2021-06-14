b08ce40a5a20b68ecb9a7bf5b853afb5fe8ebabb
afanner <fanner@wisc.edu>
2021-06-14 11:33:38 -0500

    just adding the username endpoint

--- AppBL/GACDRest/Controllers/UserController.cs
+++ AppBL/GACDRest/Controllers/UserController.cs
@@ -9,6 +9,9 @@ using Serilog;
 using GACDRest.DTO;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
+using Microsoft.Extensions.Options;
+using RestSharp;
+using Newtonsoft.Json;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace GACDRest.Controllers
@@ -18,9 +21,11 @@ namespace GACDRest.Controllers
     public class UserController : ControllerBase
     {
         private IUserBL _userBL;
-        public UserController(IUserBL userBL)
+        private readonly ApiSettings _ApiSettings;
+        public UserController(IUserBL userBL, IOptions<ApiSettings> settings)
         {
             _userBL = userBL;
+            _ApiSettings = settings.Value;
         }
         // GET: api/<UserController>
         /// <summary>
@@ -33,15 +38,38 @@ namespace GACDRest.Controllers
             return await _userBL.GetUsers();
         }
         /// <summary>
-        /// Gets a user with a given ID
+        /// Gets a username with a given user
         /// </summary>
-        /// <param name="id">Id for the user</param>
-        /// <returns>User with Id</returns>
+        /// <returns>username / name associated with user</returns>
         // GET api/<UserController>/5
-        [HttpGet("{id}")]
-        public async Task<User> GetAsync(int id)
+        [Authorize]
+        [HttpGet("username")]
+        public async Task<UserNameModel> Get()
         {
-            return await _userBL.GetUser(id);
+            try
+            {
+                string UserID = this.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+                if (await _userBL.GetUser(UserID) == null)
+                {
+                    GACDModels.User user = new GACDModels.User();
+                    user.Auth0Id = UserID;
+                    await _userBL.AddUser(user);
+                }
+                User u = await _userBL.GetUser(UserID);
+                dynamic AppBearerToken = GetApplicationToken();
+                var client = new RestClient($"https://kwikkoder.us.auth0.com/api/v2/users/{u.Auth0Id}");
+                var request = new RestRequest(Method.GET);
+                request.AddHeader("authorization", "Bearer " + AppBearerToken.access_token);
+                IRestResponse restResponse = await client.ExecuteAsync(request);
+                dynamic deResponse = JsonConvert.DeserializeObject(restResponse.Content);
+                return new UserNameModel(deResponse.username, deResponse.name);
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message);
+                Log.Error("Unexpected error occured in LBController");
+                return null;
+            }
         }
         /// <summary>
         /// Posting a new user to the db
@@ -75,6 +103,16 @@ namespace GACDRest.Controllers
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+        }
+        private dynamic GetApplicationToken()
+        {
+            var client = new RestClient("https://kwikkoder.us.auth0.com/oauth/token");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("content-type", "application/json");
+            request.AddParameter("application/json", _ApiSettings.authString, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            //Log.Information("Response: {0}",response.Content);
+            return JsonConvert.DeserializeObject(response.Content);
         }
     }
 }
---
+++ AppBL/GACDRest/DTO/UserNameModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GACDRest.DTO
+{
+    public class UserNameModel
+    {
+        public UserNameModel () { }
+        public UserNameModel (string userName, string name)
+        {
+            this.UserName = userName;
+            this.Name = name;
+        }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+    }
+}
