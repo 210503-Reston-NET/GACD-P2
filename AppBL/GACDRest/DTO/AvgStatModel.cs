4c32aef17c6299aac4defc770a9bcb1db7215f2d
afanner <fanner@wisc.edu>
2021-06-16 21:44:39 -0500

    deploying new endpoint

---
+++ AppBL/GACDRest/DTO/AvgStatModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GACDRest.DTO
+{
+    public class AvgStatModel
+    {
+        public AvgStatModel() { }
+        public int userID { get; set; }
+        public double averagewpm { get; set; }
+        public double averageaccuracy { get; set; }
+        public int numberoftests { get; set; }
+        public int totaltesttime { get; set; }
+        public int revapoints { get; set; }
+    }
+}
